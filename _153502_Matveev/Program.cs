5338be55c612f1145c43e3c34973786e3e075223
nazar <nazar@DESKTOP-MPQB9S9>
2022-03-09 22:59:34 +0300

    Add project files.

---
+++ _153502_Matveev/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.Write("Enter first number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = a / b;
+            Console.Write("Result of division: {0}", c);
+        }
+    }
+}
